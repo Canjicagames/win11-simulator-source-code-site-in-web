dbd22601348c2fcca7248f00d2fca7f34e24adb1
the devloper's studios <140197542+Canjicagames@users.noreply.github.com>
2024-05-30 15:37:47 -0300

    source code

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('#botao');
+document.addEventListener('click', function(event){
+    document.querySelector('#container01')
+        .classList.toggle('fade');
+        
+
+});
